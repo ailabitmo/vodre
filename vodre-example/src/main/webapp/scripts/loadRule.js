e665e2f12c6f8154f6cb173d2785e8f611519530
Eugene Gusarov <gusarov.email@gmail.com>
2013-12-19 02:11:30 +0400

    Issue #11: rewrite LoadRule form using AngularJS

--- vodre-example/src/main/webapp/scripts/loadRule.js
+++ vodre-example/src/main/webapp/scripts/loadRule.js
@@ -1,53 +1,51 @@
-$(document).ready(function () {
-       // var classesDescriptionNumber = $('.classDescr').size();
-    });
+var vodreApp = angular.module("vodreApp", []);
 
-function addNewField(sender){
-    btnId = $(sender).attr("id");
-    parentId = $(sender).parent('.classDescr').attr("id");
-    var fieldsNumber = $(sender).parent('.classDescr').find('.classFields').size();
-    
-    $('<p>Field: <input id="' + parentId + '_fieldName' + fieldsNumber + '" type="text">' +
-            ' Value: <input id="' + parentId + '_fieldValue' + fieldsNumber + '" type="text">' +
-            ' <input id="' + parentId + '_removeField' + fieldsNumber + '" type="button"' +
-                'value="Remove" onclick="removeField(this);"></p>')
-            .fadeIn('slow').insertBefore('#' + btnId);
+vodreApp.controller("RulesController", function($scope) {
+    $scope.classes = [newClass()];
+
+    $scope.addField = function($class) {
+        $class.fields.push(newField());
+        updateBinding();
+    };
+
+    $scope.removeField = function($field, $class) {
+        $class.fields.deleteObject($field);
+        updateBinding();
+    };
+
+    $scope.addClass = function() {
+        $scope.classes.push(newClass());
+        updateBinding();
+    };
+
+    $scope.removeClass = function($class) {
+        $scope.classes.deleteObject($class);
+        updateBinding();
+    };
+
+    function newClass() {
+        return { fields: [newField()] };
+    }
+
+    function newField() {
+        return {
+            name: "",
+            value: ""
+        };
+    }
+
+    function updateBinding() {
+        setTimeout(function() {
+            $scope.$apply();
+        }, 1);
+    }
+});
+
+Array.prototype.deleteObject = function(object) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === object) {
+            this.splice(i, 1);
+            break;
+        }
+    }
 };
-
-function addNewClass(sender){
-    btnId = $(sender).attr("id");
-    var classesDescriptionNumber = $('.classDescr').size();
-    
-    $('<div id="classDescription' + classesDescriptionNumber + '" ' +
-             'class="classDescr" style="border:1px solid black;">' +
-                    '<p>' + 
-                        'Class name' +
-                        '<input id="classDescription' + classesDescriptionNumber + '_className"' +
-                            'type="text">' +
-                            '<input id="classDescription' + classesDescriptionNumber +
-                                '_removeClass" type="button" value="Remove class" onclick="removeClass(this);">' +
-                    '</p>' +
-                    '<p class="classFields">' +
-                        'Field: ' +
-                        '<input id="classDescription' + classesDescriptionNumber + 
-                            '_fieldName0" type="text"> ' +
-                        'Value: ' +
-                        '<input id="classDescription' + classesDescriptionNumber + 
-                            '_fieldValue0" type="text"> ' +
-                        //'' +
-                    '</p>' +
-                    '<input id="classDescription' + classesDescriptionNumber +
-                        '_newField" type="button" value="New field" onclick="addNewField(this);">' +
-               '</div>')
-            .fadeIn('slow').insertBefore('#' + btnId);
-}
-
-function removeClass(sender){
-    parent = $(sender).parent().parent();
-    $(parent).fadeOut('slow', function(){$(parent).remove();});
-}
-
-function removeField(sender){
-    parent = $(sender).parent('p');
-    $(parent).fadeOut('slow', function(){$(parent).remove();});
-}
