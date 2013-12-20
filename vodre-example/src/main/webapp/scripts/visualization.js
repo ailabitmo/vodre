1789d26d24ea266b93d836135ad671a2626fa4b2
hdimon <hldimon@mail.ru>
2013-12-21 00:14:35 +0400

    Presentation of facts and rules with relations.

--- vodre-example/src/main/webapp/scripts/visualization.js
+++ vodre-example/src/main/webapp/scripts/visualization.js
@@ -2,17 +2,33 @@ var visualization = new function () {
     var self = this;
     var stage;
     var layers = {};
+    var _blocks;
+
+    var blockTypes = {
+        fact: "objectInserted",
+        rule: "ruleTriggered"
+    };
+
+    var memoryActions = {
+        add: "add",
+        remove: "remove",
+        change: "change"
+    };
 
     var settings = {
-        canvasWidth:600,
-        canvasHeight:200,
-        blockWidth:300,
-        blockHeight:50,
-        blockLeftMargin:50,
-        blockTopMargin:50,
-        fontSize:14,
-        fontWidth:6,
-        lineHeight:15
+        canvasWidth: 600,
+        canvasHeight: 200,
+        blockWidth: 300,
+        blockHeight: 50,
+        blockLeftMargin: 50,
+        blockTopMargin: 50,
+        timeLineLeftMargin: 10,
+        fontSize: 14,
+        fontWidth: 6,
+        lineHeight: 15,
+        rulePolygonIncline: 10, //different in pixels between topLeftCorner and lowerLeftCorner
+        arrowWidth: 10,
+        arrowHeight: 5
     };
 
     var lastBlockMarginTop = 0;
@@ -24,49 +40,174 @@ var visualization = new function () {
 
     var initKinetic = function () {
         stage = new Kinetic.Stage({
-            width:settings.canvasWidth,
-            height:settings.canvasHeight,
-            container:'droolsVisualizationCanvas'
+            width: settings.canvasWidth,
+            height: settings.canvasHeight,
+            container: 'droolsVisualizationCanvas'
         });
         layers.main = new Kinetic.Layer();
         layers.texts = new Kinetic.Layer();
+        layers.timeLine = new Kinetic.Layer();
+        layers.memoryActionLines = new Kinetic.Layer();
+        layers.calledRulesLines = new Kinetic.Layer();
         stage.add(layers.main);
         stage.add(layers.texts);
+        stage.add(layers.timeLine);
+        stage.add(layers.memoryActionLines);
+        stage.add(layers.calledRulesLines);
     };
 
     this.drawBlocks = function (blocks) {
-        blocks.forEach(function (block) {
-            self.addBlock(block);
-        });
+        _blocks = blocks;
+
+        drawBlock(0);
     };
 
-    this.addBlock = function (block) {
-        var textLines = getTextLines("Name: " + block.name + "\nType: " + block.type);
+    var drawBlock = function (indexBlock) {
+        var block = _blocks[indexBlock];
+        if (!block)
+            return;
+        switch (block.type) {
+            case blockTypes.fact:
+                self.addFact(block);
+                break;
+            case blockTypes.rule:
+                self.addRule(block);
+                break;
+        }
+
+        setTimeout(function () {
+            drawBlock(indexBlock + 1);
+        }, 0);
+    };
+
+    this.addFact = function (block) {
+        var textLines = getTextLines("Name: " + block.name);
         var blockHeight = getBlockHeight(textLines);
 
         var newBlockMarginTop = getNewBlockMarginTop(blockHeight);
 
-        if (layers.main.getChildren().length > 0)
-            addArrow(lastBlockMarginTop, newBlockMarginTop);
-
         lastBlockMarginTop = newBlockMarginTop;
+        block.blockMarginTop = newBlockMarginTop;
 
         resizeCanvas(newBlockMarginTop);
+        drawTimeLine();
+        drawFactRectangle(blockHeight, newBlockMarginTop);
+
+        lastBlockHeight = blockHeight;
+        block.blockHeight = blockHeight;
+
+        drawMemoryAction(block);
+
+        if (block.calledRule)
+            addArrow(block);
+
+        addText(textLines, newBlockMarginTop);
+    };
+
+    var drawFactRectangle = function (blockHeight, blockMarginTop) {
         var rect = new Kinetic.Rect({
-            width:settings.blockWidth,
-            height:blockHeight,
-            x:settings.blockLeftMargin,
-            y:newBlockMarginTop,
-            fill:'lightgreen',
-            stroke:'black',
-            strokeWidth:1
+            width: settings.blockWidth,
+            height: blockHeight,
+            x: settings.blockLeftMargin,
+            y: blockMarginTop,
+            fill: 'lightgreen',
+            stroke: 'black',
+            strokeWidth: 1
         });
 
         layers.main.add(rect);
         layers.main.batchDraw();
+    };
+
+    this.addRule = function (block) {
+        var textLines = getTextLines("Name: " + block.name);
+        var blockHeight = getBlockHeight(textLines);
+
+        var newBlockMarginTop = getNewBlockMarginTop(blockHeight);
+
+        lastBlockMarginTop = newBlockMarginTop;
+        block.blockMarginTop = newBlockMarginTop;
+
+        resizeCanvas(newBlockMarginTop);
+        drawTimeLine();
+        drawRulePolygon(blockHeight, newBlockMarginTop);
+
         lastBlockHeight = blockHeight;
+        block.blockHeight = blockHeight;
 
         addText(textLines, newBlockMarginTop);
+    };
+
+    var drawRulePolygon = function (blockHeight, blockMarginTop) {
+        var polygon = new Kinetic.Polygon({
+            points: [settings.blockLeftMargin + settings.rulePolygonIncline, blockMarginTop,
+                settings.blockLeftMargin + settings.blockWidth, blockMarginTop,
+                settings.blockLeftMargin + settings.blockWidth - settings.rulePolygonIncline, blockMarginTop + blockHeight,
+                settings.blockLeftMargin, blockMarginTop + blockHeight],
+            fill: 'lightgreen',
+            stroke: 'black',
+            strokeWidth: 1
+        });
+
+        layers.main.add(polygon);
+        layers.main.batchDraw();
+    };
+
+    var drawMemoryAction = function (block) {
+        var topMargin = block.blockMarginTop + block.blockHeight / 2;
+
+        drawHorizontLine([settings.timeLineLeftMargin, topMargin, settings.blockLeftMargin, topMargin ]);
+
+        switch (block.memoryAction) {
+            case memoryActions.add:
+                drawLeftArrow(settings.timeLineLeftMargin, topMargin);
+                break;
+            case memoryActions.remove:
+                drawRightArrow(settings.blockLeftMargin, topMargin);
+                break;
+            case memoryActions.change:
+                drawLeftArrow(settings.timeLineLeftMargin, topMargin);
+                drawRightArrow(settings.blockLeftMargin, topMargin);
+                break;
+        }
+    };
+
+    var drawHorizontLine = function (pointsArray) {
+        var line = new Kinetic.Line({
+            points: pointsArray,
+            stroke: 'black'
+        });
+
+        layers.memoryActionLines.add(line);
+        layers.memoryActionLines.batchDraw();
+    };
+
+    var drawLeftArrow = function (leftPointX, leftPointY) {
+        var polygon = new Kinetic.Polygon({
+            points: [leftPointX, leftPointY,
+                leftPointX + settings.arrowWidth, leftPointY + settings.arrowHeight,
+                leftPointX + settings.arrowWidth, leftPointY - settings.arrowHeight],
+            fill: 'black',
+            stroke: 'black',
+            strokeWidth: 1
+        });
+
+        layers.memoryActionLines.add(polygon);
+        layers.memoryActionLines.batchDraw();
+    };
+
+    var drawRightArrow = function (rightPointX, rightPointY) {
+        var polygon = new Kinetic.Polygon({
+            points: [rightPointX, rightPointY,
+                rightPointX - settings.arrowWidth, rightPointY + settings.arrowHeight,
+                rightPointX - settings.arrowWidth, rightPointY - settings.arrowHeight],
+            fill: 'black',
+            stroke: 'black',
+            strokeWidth: 1
+        });
+
+        layers.memoryActionLines.add(polygon);
+        layers.memoryActionLines.batchDraw();
     };
 
     var getTextLines = function (text) {
@@ -78,7 +219,6 @@ var visualization = new function () {
         for (var i = 0; i < words.length; i++) {
             var testLine = line + words[i] + ' ';
             var metrics = context.measureText(testLine);
-            //var testWidth = metrics.width;
             var testWidth = testLine.length * settings.fontWidth;
             if (testWidth > settings.blockWidth - 20) {
                 lines.push(line);
@@ -99,7 +239,6 @@ var visualization = new function () {
 
         textLines.forEach(function (line) {
             var lineHeight = getLineHeight(line);
-
             textHeight += lineHeight;
         });
 
@@ -129,19 +268,57 @@ var visualization = new function () {
     };
 
     var resizeCanvas = function (marginTop) {
-        if (marginTop + settings.blockHeight > settings.canvasHeight)
-            stage.setSize(settings.canvasWidth, marginTop + settings.blockHeight + settings.blockTopMargin);
+        if (marginTop + settings.blockHeight > settings.canvasHeight) {
+            settings.canvasHeight = marginTop + settings.blockHeight + settings.blockTopMargin;
+            stage.setSize(settings.canvasWidth, settings.canvasHeight);
+        }
     };
 
-    var addArrow = function (lastBlockMarginTop, newBlockMarginTop) {
-        var leftMargin = settings.blockLeftMargin + settings.blockWidth / 2;
+    var drawTimeLine = function () {
+        var line = layers.timeLine.getChildren()[0];
+        if (!line) {
+            line = new Kinetic.Line({
+                points: [settings.timeLineLeftMargin, 0, settings.timeLineLeftMargin, settings.canvasHeight],
+                stroke: 'black'
+            });
 
-        var line = new Kinetic.Line({
-            points:[leftMargin, lastBlockMarginTop + lastBlockHeight, leftMargin, newBlockMarginTop],
-            stroke:'black'
+            layers.timeLine.add(line);
+        } else {
+            line.setPoints([settings.timeLineLeftMargin, 0, settings.timeLineLeftMargin, settings.canvasHeight]);
+        }
+
+        layers.timeLine.batchDraw();
+    };
+
+    var addArrow = function (block) {
+        var calledRule = getCalledRule(block.calledRule);
+        var distance = block.blockMarginTop - calledRule.blockMarginTop;
+        var topBlockCenterY = calledRule.blockMarginTop + calledRule.blockHeight / 2;
+        var lowerBlockCenterY = block.blockMarginTop + block.blockHeight / 2;
+
+        var spline = new Kinetic.Spline({
+            points: [
+                {
+                    x: settings.blockLeftMargin + settings.blockWidth - settings.rulePolygonIncline / 2,
+                    y: topBlockCenterY
+                },
+                {
+                    x: settings.blockLeftMargin + settings.blockWidth + distance * 0.3,
+                    y: topBlockCenterY + distance / 2
+                },
+                {
+                    x: settings.blockLeftMargin + settings.blockWidth,
+                    y: lowerBlockCenterY
+                }
+            ],
+            stroke: 'black',
+            strokeWidth: 2,
+            tension: 1
         });
 
-        layers.main.add(line);
+        layers.calledRulesLines.add(spline);
+        layers.calledRulesLines.batchDraw();
+        drawLeftArrow(settings.blockLeftMargin + settings.blockWidth, lowerBlockCenterY);
     };
 
     var addText = function (textLines, newBlockMarginTop) {
@@ -150,12 +327,12 @@ var visualization = new function () {
 
         textLines.forEach(function (line) {
             var text = new Kinetic.Text({
-                x:leftMargin,
-                y:newBlockMarginTop + topMargin,
-                text:line,
-                fontSize:settings.fontSize,
-                fontFamily:'Calibri',
-                fill:'green'
+                x: leftMargin,
+                y: newBlockMarginTop + topMargin,
+                text: line,
+                fontSize: settings.fontSize,
+                fontFamily: 'Calibri',
+                fill: 'green'
             });
 
             layers.texts.add(text);
@@ -164,5 +341,11 @@ var visualization = new function () {
         });
 
         layers.texts.batchDraw();
+    };
+
+    var getCalledRule = function (calledRuleName) {
+        return _blocks.filter(function (block) {
+            return block.name === calledRuleName;
+        })[0];
     };
 }()
--- vodre-example/src/main/webapp/scripts/vodre.js
+++ vodre-example/src/main/webapp/scripts/vodre.js
@@ -1,5 +1,5 @@
 var vodre = new function () {
-    //extarnal dependencies
+    //external dependencies
     var _visualization = visualization;
 
     $(document).ready(function () {
@@ -7,13 +7,36 @@ var vodre = new function () {
     });
 
     var init = function () {
-        getBlocks(function(blocks) {
-            visualization.drawBlocks(blocks);
+        getBlocks(function (blocks) {
+            _visualization.drawBlocks(blocks);
         });
     };
 
     var getBlocks = function (callback) {
-        $.get("./rest/", function(blocks) {
+        $.get("./rest/", function (blocks) {
+            //test data
+            blocks[0].memoryAction = "remove";
+            blocks[1].memoryAction = "add";
+            blocks[3] = {
+                name: "fact3",
+                type: "objectInserted",
+                memoryAction: "change",
+                calledRule: "Rule #1"
+            };
+            blocks[4] = {
+                name: "fact4",
+                type: "objectInserted",
+                memoryAction: "add",
+                calledRule: "Rule #1"
+            };
+            blocks[5] = {
+                name: "fact5",
+                type: "objectInserted",
+                memoryAction: "change",
+                calledRule: "Rule #1"
+            };
+            //test data
+
             callback(blocks);
         });
     };
